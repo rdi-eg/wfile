e607cd8459cb154ac3cdbcc16265de149ab34464
Adham Zahran <adham.zahran@hotmail.com>
2018-04-22 13:24:32 +0200

    added identation for namespace

--- rdi_wfile.h
+++ rdi_wfile.h
@@ -22,33 +22,35 @@
 
 extern "C++"
 {
-	namespace RDI
-	{
-		std::wstring read_wfile(const std::string &filename);
-		std::vector<std::wstring> read_wfile_lines(const std::string &filename);
-		bool write_wfile(const std::string &filename, const std::wstring &fileContent);
-		bool append_to_wfile(const std::string &filename, const std::wstring &content);
-		bool write_wfile_lines(const std::string& filename, const std::vector<std::wstring>& linesToWrite);
-		std::string read_file(const std::string& filename);
-		std::vector<std::string> read_file_lines(const std::string& filename);
-		bool write_file(const std::string& filename, const std::string& fileContent);
-		bool append_to_file(const std::string& filename, const std::string& content);
-		bool write_file_lines(const std::string& filename, const std::vector<std::string>& linesToWrite);
-		bool delete_file(const std::string& path);
-		std::string get_absolute_path(std::string path);
-		std::string get_current_directory(); // returns the location of the binary executable ex: "/home/rdi/bin"
-		std::vector<std::string> get_directory_content(const std::string& path);
-		bool create_directory(std::string path);
-		bool delete_directory(const std::string& path);
-		std::map<std::string , std::string> parse_config_file(const std::string& filename);
-        bool dump_matrix(const std::string &file_name , std::vector<std::vector<float>> &input_matrix);
-        bool file_exists(const std::string &file_name);
-		bool is_directory(const std::string& path);
-		std::string extract_path_from_filename(std::string filename);
-		std::string extract_filename(std::string path);
-		std::string extract_filename_without_extension(std::string path);
-		std::string extract_extenstion_from_path(const std::string& filename);
-	} // namespace RDI
+namespace RDI
+    {
+
+    std::wstring read_wfile(const std::string &filename);
+    std::vector<std::wstring> read_wfile_lines(const std::string &filename);
+    bool write_wfile(const std::string &filename, const std::wstring &fileContent);
+    bool append_to_wfile(const std::string &filename, const std::wstring &content);
+    bool write_wfile_lines(const std::string& filename, const std::vector<std::wstring>& linesToWrite);
+    std::string read_file(const std::string& filename);
+    std::vector<std::string> read_file_lines(const std::string& filename);
+    bool write_file(const std::string& filename, const std::string& fileContent);
+    bool append_to_file(const std::string& filename, const std::string& content);
+    bool write_file_lines(const std::string& filename, const std::vector<std::string>& linesToWrite);
+    bool delete_file(const std::string& path);
+    std::string get_absolute_path(std::string path);
+    std::string get_current_directory(); // returns the location of the binary executable ex: "/home/rdi/bin"
+    std::vector<std::string> get_directory_content(const std::string& path);
+    bool create_directory(std::string path);
+    bool delete_directory(const std::string& path);
+    std::map<std::string , std::string> parse_config_file(const std::string& filename);
+    bool dump_matrix(const std::string &file_name , std::vector<std::vector<float>> &input_matrix);
+    bool file_exists(const std::string &file_name);
+    bool is_directory(const std::string& path);
+    std::string extract_path_from_filename(std::string filename);
+    std::string extract_filename(std::string path);
+    std::string extract_filename_without_extension(std::string path);
+    std::string extract_extenstion_from_path(const std::string& filename);
+
+    } // namespace RDI
 }
 
 #endif // RDI_WFILEIO_H
